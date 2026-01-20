a12d73900d480d070c5fda4e6a41bb9bf7af3c36
Nicole <moura2007nick@gmail.com>
2026-01-19 21:21:05 -0300

    adicionando arquivos

---
+++ app.py
@@ -0,0 +1,13 @@
+print('Sabor Express')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+input('Escolha uma opção: ')
+
+# código omitido…
+
+opcao_escolhida = input('Escolha uma opção: ')
+print('Você escolheu a opção', opcao_escolhida)
