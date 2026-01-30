8fb0567e8f9ac010c2fb2d5cebc43bda21f3ed6b
Nicole Moura <moura2007nick@gmail.com>
2026-01-30 08:50:34 -0300

    atualizando login

--- ProjetoPython/Desktop/login.py
+++ ProjetoPython/Desktop/login.py
@@ -15,5 +15,54 @@ def validaCampos():
 
 def login(usuario, senha):
 
-    if usuario == "admin" and senha == "pass123$"
-        QMessageBox.information(telaLogin)   
+    if usuario == "admin" and senha == "pass123$":
+        QMessageBox.information(telaLogin, "Falha no login", "Usuário ou senha inválidos.")
+
+#Limpar Campos
+def limpaCampos():
+
+    caixaTextoUsuario.clear()
+    caixaTextoSenha.clear()
+    caixaTextoUsuario.setFocus() 
+
+
+
+#Criando aplicação
+app = QApplication(sys.argv) 
+
+#Janela
+telaLogin = QWidget()
+telaLogin.setWindowTitle("Login")
+telaLogin.setGeometry(100, 100, 300, 200)
+
+#Rótulo(label)
+#Usuário
+textoRotuloUsuario = QLabel('Usuário:', telaLogin)
+textoRotuloUsuario.move(80, 30)
+#Senha
+textoRotuloSenha = QLabel('Senha: ', telaLogin)
+textoRotuloSenha.move(80, 80)
+
+
+#Criando caixa de texto
+#Usuário
+caixaTextoUsuario = QLineEdit(telaLogin)
+caixaTextoUsuario.move(80, 50)
+#Senha
+caixaTextoSenha = QLineEdit(telaLogin)
+caixaTextoSenha.setEchoMode(QLineEdit.Password)
+caixaTextoSenha.move(80, 100)
+
+
+#Criando um botão
+botao = QPushButton('Entrar', telaLogin)
+botao.move(102, 140)
+
+#conexão do banco a função
+botao.clicked.connect(validaCampos)
+
+#Exibindo a janela
+telaLogin.show()
+
+#Iniciando o loop de eventos
+sys.exit(app.exec_())
